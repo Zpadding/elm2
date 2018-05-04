17812ac9bf7503c7b64e2b6552780846194189ac
padding <849119555@qq.com>
2018-05-04 10:05:22 +0800

    基本框架搭建

---
+++ vue/app/src/components/headerView/HeaderView.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="headerView">
+    <ul>
+      <li v-for="mes in mess">
+        <router-link :to="'/show'+mes.to">{{mes.name}}</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "HeaderView",
+    data() {
+      return {
+        mess: [
+          {name: "美食", to: "/eat"},
+          {name: "搜索", to: "/search"},
+          {name: "订单", to: "/order"},
+          {name: "我的", to: "/user"},
+
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .headerView ul {
+    display: flex;
+    justify-content: space-around;
+    background: yellowgreen;
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
+  .headerView ul li {
+    height: .5rem;
+    display: flex;
+    align-items: center;
+  }
+  .headerView ul li a {
+    color: #000;
+  }
+
+</style>
---
+++ vue/app/src/components/show/Show.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <HeaderView></HeaderView>
+    <router-view/>
+  </div>
+
+</template>
+
+<script>
+  import HeaderView from "../headerView/HeaderView";
+
+  export default {
+    name: "Show",
+    components: {HeaderView}
+  }
+</script>
+
+<style scoped>
+
+</style>
