ade1db007962a631328ff788be3e953b9c164b3b
1Students <18738187031@163.com>
2018-05-04 11:41:41 +0800

    user 编写 5.4上午

--- vue/app/src/components/user/User.vue
+++ vue/app/src/components/user/User.vue
@@ -1,7 +1,25 @@
 <template>
-    <div>
-      <h1>用户界面</h1>
+    <div id="User">
+      <div id="head">
+
+        <p class="hep1"> < </p>
+
+        <p class="hep2">我的</p>
+
+      <div >
+        <img src="../../assets/w1-4.png" alt="" class="in1">
+       <p>登录/注册</p>
+        <span><img src="../../assets/w1-5.png" alt="" class="">
+          <p>暂无手机号</p>
+          <p> > </p>
+        </span>
+      </div>
+      </div>
+
+
+
     </div>
+
 </template>
 
 <script>
@@ -11,5 +29,25 @@
 </script>
 
 <style scoped>
+#head{
+  height: 1.5rem;
+  width: 100%;
+  background: cornflowerblue;
+  color: whitesmoke;
+  overflow: hidden;
+}
+  .hep1{
+    float: left;
+    padding: 0.1rem;
+  }
+  .hep2{
+    float:right;
+    padding-right: 1.5rem;
+    padding-top: 0.1rem;
+  }
 
+  .in1{
+    height: 0.5rem;
+    width: 0.5rem;
+  }
 </style>
