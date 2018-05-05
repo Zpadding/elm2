0a9bad03f132d3d7bd5c24eb0bfb8cd96133b112
padding <849119555@qq.com>
2018-05-05 11:59:53 +0800

    请求到了商铺详情,并作了展示,5月5号上午

--- vue/app/src/components/eat/shop/Shop.vue
+++ vue/app/src/components/eat/shop/Shop.vue
@@ -6,22 +6,28 @@
             <span>附近商家</span>
           </div>
           <div class="body">
-              <router-link to="/show">
+              <router-link to="/show" v-for="mes in mess">
                 <div class="pic">
-                    <img class="logo" :src="logo" alt="">
+                    <img class="logo" :src="img_url+mes.image_path" alt="">
                 </div>
                 <div class="con">
                     <div class="one">
                         <div class="left">
-                            <mark>品牌</mark>
-                            <span>效果演示</span>
+                            <nobr>
+                                <span>品牌</span>
+                                <span>{{mes.name}}</span>
+                            </nobr>
                         </div>
-                        <div class="right">保准票</div>
+                        <ul class="right">
+                            <li>保</li>
+                            <li>准</li>
+                            <li>票</li>
+                        </ul>
                     </div>
                     <div class="two">
                         <div class="left">
-                            <span>评分</span>
-                            <span>月售106单</span>
+                            <span>评分{{mes.rating}}</span>
+                            <span>月售{{mes.recent_order_num}}单</span>
                         </div>
                         <div class="right">
                             <span>蜂鸟专送</span>
@@ -33,8 +39,8 @@
                             <span>¥20起送/配送费约¥5</span>
                         </div>
                         <div class="right">
-                            <span>1444.2公里/</span>
-                            <span>17小时57分钟</span>
+                            <span>{{mes.distance}}/</span>
+                            <span>{{mes.order_lead_time}}</span>
                         </div>
                     </div>
                 </div>
@@ -46,61 +52,126 @@
 </template>
 
 <script type="text/ecmascript-6">
-import logo from '../../../assets/z-3.jpg'
+import logo from "../../../assets/z-3.jpg";
+import {mapState} from 'vuex';
 export default {
   data() {
     return {
-        logo: logo
-    }
+      logo: logo,
+      mess: [],
+      img_url: "//elm.cangdu.org/img/"
+    };
   },
-  components: {
-
+  components: {},
+  created() {
+      let url = this.head_url+"/shopping/restaurants";
+      let params = {
+          latitude: 31.22967,
+          longitude: 121.4762
+      };
+      this.$http.get(url, {
+          params: params
+      }).then((result) => {
+          console.log(result.data);
+          this.mess = result.data;
+      }).catch((err) => {
+          console.log(err);
+      });
+  },
+  computed: {
+      ...mapState(["head_url"]),
   }
-}
+};
 </script>
 
 <style scoped>
 .shop {
-    margin-top: .08rem;
-    font-size: .12rem;
-    color: #999;
+  margin-top: 0.08rem;
+  font-size: 0.12rem;
+  border-top: 0.01rem solid #e3e3e3;
+  color: #999;
 }
 .head {
-    width: 3.2rem;
-    height: 0.33rem;
-    display: flex;
-    align-items: center;
+  width: 3.2rem;
+  height: 0.33rem;
+  display: flex;
+  align-items: center;
 }
 .head .logo {
-    width: 0.12rem;
-    height: 0.12rem;
-    margin: 0 .07rem 0 .12rem;
+  width: 0.12rem;
+  height: 0.12rem;
+  margin: 0 0.07rem 0 0.12rem;
 }
-.body a{
-    padding: .14rem .08rem;
-    display: flex;
-    justify-content: space-around;
-    color: #999;
+.body a {
+  padding: 0.14rem 0.08rem;
+  display: flex;
+  justify-content: space-around;
+  color: #999;
+  border-bottom: 0.01rem solid #e3e3e3;
 }
 .body .left {
-    margin: 0 .2rem;
-    max-width: 1rem;
+  margin: 0 0.2rem;
+  max-width: 1rem;
 }
 .body .right {
-    max-width: 1rem;
+  max-width: 1.1rem;
 }
-.one, .two, .three {
-    overflow: hidden;
-    margin-bottom: .1rem;
+.one,
+.two,
+.three {
+  overflow: hidden;
+  margin-bottom: 0.1rem;
 }
 .body .pic .logo {
-    width: 0.54rem;
-    height: 0.54rem;
+  width: 0.54rem;
+  height: 0.54rem;
 }
 .one .left {
-    font-size: .13rem;
+  font-size: 0.13rem;
+  /* height: 0.12rem; */
+  /* line-height: .12rem; */
+}
+.one .left span:first-child {
+  width: .24rem;
+  background: #ffd930;
+  padding: 0 0.03rem;
+  border-radius: 0.05rem;
+}
+.one .right {
+  display: flex;
+}
+.one .right li {
+  margin-left: 0.01rem;
+  padding: 0 0.01rem;
+  border: 0.01rem solid #f1f1f1;
+  border-radius: .03rem;
 }
 .two .left {
-    font-weight: 200;
+  font-weight: 200;
+}
+.two .right span {
+  display: inline-block;
+  height: 0.13rem;
+  line-height: 0.13rem;
+}
+.two .right span:first-child {
+  width: 0.48rem;
+  background: #3190e8;
+  color: white;
+  border: 0.01rem solid #3190e8;
+  border-radius: 0.02rem;
+  font-weight: 100;
+  /* font-family: STkaiti; */
+}
+.two .right span:nth-of-type(2) {
+  /* background: #3190e8; */
+  width: 0.36rem;
+  color: #3190e8;
+  border: 0.01rem solid #3190e8;
+  border-radius: 0.02rem;
+  font-weight: 100;
+}
+.three .right span:nth-of-type(2) {
+  color: #3190e8;
 }
 </style>
--- vue/app/src/components/headerView/HeaderView.vue
+++ vue/app/src/components/headerView/HeaderView.vue
@@ -29,7 +29,7 @@
     display: flex;
     justify-content: space-around;
     background: yellowgreen;
-    position: absolute;
+    position: fixed;
     left: 0;
     right: 0;
     bottom: 0;
